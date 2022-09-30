d572cefb7928bbf4f53609f7c5114f0a767304c9
Anantha S N <ananthasn@Ananthas-MacBook-Pro.local>
2022-09-29 19:46:56 -0700

    Analytics part 2

--- Ice breaker/Assets/Scripts/AnalyticsManager.cs
+++ Ice breaker/Assets/Scripts/AnalyticsManager.cs
@@ -8,7 +8,7 @@ public class AnalyticsManager : MonoBehaviour
 {
     public static AnalyticsManager instance;
     [SerializeField] private string URL = @"https://docs.google.com/forms/d/1sVwYPrYkX4fS6puE_-Oc7hU7ZfxaORwZhxLaNZ2hFK8/formResponse";
-    private int _sessionID;
+    public int _sessionID;
     private int _levelId;
     private int _completed ;
     public int ballReset =0;
@@ -24,6 +24,11 @@ public class AnalyticsManager : MonoBehaviour
         _levelId = level;
         _completed = complete;
         StartCoroutine(Post(_sessionID.ToString(), _levelId.ToString(), _completed.ToString()));
+    }
+    public void Send2(int level, int bricks)
+    {
+        _levelId = level;
+        StartCoroutine(Post2(_sessionID.ToString(), _levelId.ToString(), bricks));
     }
     private IEnumerator Post(string sessionID, string levelId, string completed)
     {
@@ -43,6 +48,28 @@ public class AnalyticsManager : MonoBehaviour
             else
             {
             Debug.Log("Form upload complete!");
+            }
+        }
+    }
+
+    private IEnumerator Post2(string sessionID, string levelId, int bricks )
+    {
+        string url2 = @"https://docs.google.com/forms/d/e/1FAIpQLScz7lvjsuysxh04EtFZksBaAQ73MFVlr_wlKz-s34Pwnf1xVQ/formResponse";
+        WWWForm form = new WWWForm();
+        form.AddField("entry.908848918", sessionID);
+        form.AddField("entry.1907133741", levelId.ToString());
+        form.AddField("entry.1724795905", bricks.ToString());
+        // Send responses and verify result
+        using (UnityWebRequest www = UnityWebRequest.Post(url2, form))
+        {
+        yield return www.SendWebRequest();
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+            Debug.Log(www.error);
+            }
+            else
+            {
+            Debug.Log("Form2 upload complete!");
             }
         }
     }
