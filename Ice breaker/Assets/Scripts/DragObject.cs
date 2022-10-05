07a165fbe7762066d35605aa1d1d1def91950396
Anantha S N <ananthasn@Ananthas-MacBook-Pro.local>
2022-10-04 17:06:18 -0700

    Drag option

---
+++ Ice breaker/Assets/Scripts/DragObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float zCord;
+
+    void OnMouseDown(){
+        zCord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = gameObject.transform.position - GetMosPosition();
+    }
+    private Vector3 GetMosPosition(){
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = zCord;
+        return Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+    void OnMouseDrag(){
+        transform.position = GetMosPosition() + mOffset;
+    }
+}
