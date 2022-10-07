1c343a0fadff9f9804cc31e99f9b99a055d840c7
Abhilash BS <bhaktara@usc.edu>
2022-10-07 00:26:34 -0700

    moving bricks and sticky paddle

--- Ice breaker/Assets/Scripts/wormhole.cs
+++ Ice breaker/Assets/Scripts/wormhole.cs
@@ -2,32 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class wormhole : MonoBehaviour
+public class Wormhole : MonoBehaviour
 {
-    private float wormholeCooldownTime = 0.5f;
+    private float wormholeCooldownTime = 0.2f;
     static private float nextCollisionMinTime;
-    void Start()
-    {
+    static private GameObject teleportedWormhole = null;
+    public bool outgoingCollision = false;
+    
+    void Start(){
         nextCollisionMinTime = 0;
     }
 
+    private GameObject level_16_get_other_wormhole(GameObject gameObject){
+        GameObject otherWormhole = GameObject.FindWithTag("level-16-bottomrightWormhole");
+        if(gameObject.tag == "level-16-topleftWormhole"){
+            otherWormhole = GameObject.FindWithTag("level-16-bottomrightWormhole");
+        }
+        else if(gameObject.tag == "level-16-toprightWormhole"){
+            otherWormhole = GameObject.FindWithTag("level-16-bottomleftWormhole");
+        }
+        else if(gameObject.tag == "level-16-bottomleftWormhole"){
+            otherWormhole = GameObject.FindWithTag("level-16-toprightWormhole");
+        }
+        else{
+            otherWormhole = GameObject.FindWithTag("level-16-topleftWormhole");
+        }
+        return otherWormhole;
+    }
+
+    private GameObject level_16_get_src_wormholes(GameObject gameObject)
+    {
+        if(gameObject.tag == "level-16-bottomleftWormhole"){
+            return GameObject.FindWithTag("level-16-toprightWormhole");
+        }
+        else{
+            return GameObject.FindWithTag("level-16-bottomrightWormhole");
+        }
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(Time.time);
-        if(Time.time > nextCollisionMinTime){
+        Debug.Log("on collision: ");
+        if(teleportedWormhole != null){
+            teleportedWormhole = null;
+            return;
+        }
+
+        if (collision.gameObject.name == "Ball" ){
+            Debug.Log(Time.time);
             Ball ball = collision.gameObject.GetComponent<Ball>();
-            GameObject otherWormhole;
-
-            if(gameObject.tag == "leftWormhole"){
-                otherWormhole = GameObject.FindWithTag("rightWormhole");
-            }
-            else{
-                otherWormhole = GameObject.FindWithTag("leftWormhole");
-            }
-
             if (ball != null)
             {   
-
+                GameObject otherWormhole = level_16_get_other_wormhole(gameObject);
                 float newAngle = -1 * Vector2.SignedAngle(Vector2.up, ball.rigidbody.velocity);
 
                 Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
@@ -38,16 +65,64 @@ public class wormhole : MonoBehaviour
 
                 ball.transform.position = otherWormhole.transform.position;
 
-                if (ball.GetComponent<SpriteRenderer>().color == black_color)
-                {
+                if (ball.GetComponent<SpriteRenderer>().color == black_color){
                     ball.GetComponent<SpriteRenderer>().color = white_color; 
                 }
-                else
-                {
+                else{
                     ball.GetComponent<SpriteRenderer>().color = black_color;
                 }
                 nextCollisionMinTime = Time.time + wormholeCooldownTime;
+                teleportedWormhole = this.gameObject;
+            }
+        }
+        
+        // level-16 wormhole config
+        Debug.Log(collision.gameObject.name);
+        if(collision.gameObject.tag == "Brick"){
+                Brick brick = collision.gameObject.GetComponent<Brick>();
+                if (brick != null)
+                {   
+                    if (true ){
+                            GameObject otherWormhole = level_16_get_other_wormhole(gameObject);
+                            //Destroy(brick.gameObject);
+                            collision.gameObject.transform.position = otherWormhole.transform.position;
+
+                            float newAngle = Vector2.SignedAngle(Vector2.up, brick.GetComponent<Rigidbody2D>().velocity);
+
+                            Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+                            brick.GetComponent<Rigidbody2D>().velocity = rotation * Vector2.up * brick.GetComponent<Rigidbody2D>().velocity.magnitude;
+                            nextCollisionMinTime = Time.time + wormholeCooldownTime;
+                            teleportedWormhole = this.gameObject;
+                    }
+                }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(outgoingCollision){
+            outgoingCollision = false;
+            return;
+        }
+    
+        if(collision.gameObject.tag == "Brick"){
+            Brick brick = collision.gameObject.GetComponent<Brick>();
+            if (brick != null)
+            {   
+                if (true){
+                    GameObject otherWormhole = level_16_get_other_wormhole(gameObject);
+                    //Destroy(brick.gameObject);
+                    collision.gameObject.transform.position = otherWormhole.transform.position;
+
+                    float newAngle = Vector2.SignedAngle(Vector2.up, brick.GetComponent<Rigidbody2D>().velocity);
+
+                    Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+                    brick.GetComponent<Rigidbody2D>().velocity = rotation * Vector2.up * brick.GetComponent<Rigidbody2D>().velocity.magnitude;
+                    nextCollisionMinTime = Time.time + wormholeCooldownTime;
+                    otherWormhole.GetComponent<Wormhole>().outgoingCollision = true;
+                }
             }
         }
     }
+
 }
