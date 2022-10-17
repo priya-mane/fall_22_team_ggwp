23b5e8cd33667765115c71eebf24c51be5a19411
Abhilash BS <bhaktara@usc.edu>
2022-10-17 00:23:40 -0700

    ball wormhole

--- Ice breaker/Assets/Scripts/wormhole.cs
+++ Ice breaker/Assets/Scripts/wormhole.cs
@@ -49,4 +49,42 @@ public class wormhole : MonoBehaviour
         }
     
     }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(outGoing > 0){
+            outGoing -= 1;
+            return;
+        }  
+
+        wormhole succWormhole = GameObject.FindWithTag(successorWormholeTagName).gameObject.GetComponent<wormhole>();
+        succWormhole.outGoing += 1;     
+
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+        if (ball != null)
+        {   
+            float newAngle = -1 * Vector2.SignedAngle(Vector2.up, ball.rigidbody.velocity);
+
+            Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+            ball.rigidbody.velocity = rotation * Vector2.up * ball.rigidbody.velocity.magnitude;
+
+            Color black_color = new Color(0f, 0f, 0f, 1f);
+            Color white_color = new Color(1f, 1f, 1f, 1f);
+
+            ball.transform.position = succWormhole.transform.position;
+
+            if (ball.GetComponent<SpriteRenderer>().color == black_color) {
+                ball.GetComponent<SpriteRenderer>().color = white_color; 
+            }
+            else {
+                ball.GetComponent<SpriteRenderer>().color = black_color;
+            }
+        }
+
+        Brick brick = collision.gameObject.GetComponent<Brick>();
+        if (brick != null)
+        {
+            brick.transform.position = succWormhole.transform.position;
+        }
+    
+    }
 }
