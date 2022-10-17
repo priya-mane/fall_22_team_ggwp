c91d8529682ad58f84b6e278892f2c52ce03bf75
Abhilash BS <bhaktara@usc.edu>
2022-10-16 23:54:57 -0700

    Wormhole changes

--- Ice breaker/Assets/Scripts/wormhole.cs
+++ Ice breaker/Assets/Scripts/wormhole.cs
@@ -4,50 +4,49 @@ using UnityEngine;
 
 public class wormhole : MonoBehaviour
 {
-    private float wormholeCooldownTime = 0.5f;
-    static private float nextCollisionMinTime;
+    private int outGoing;
+    public string successorWormholeTagName;
     void Start()
     {
-        nextCollisionMinTime = 0;
+        outGoing = 0;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(Time.time);
-        if(Time.time > nextCollisionMinTime){
-            Ball ball = collision.gameObject.GetComponent<Ball>();
-            GameObject otherWormhole;
+        if(outGoing > 0){
+            outGoing -= 1;
+            return;
+        }  
 
-            if(gameObject.tag == "leftWormhole"){
-                otherWormhole = GameObject.FindWithTag("rightWormhole");
+        wormhole succWormhole = GameObject.FindWithTag(successorWormholeTagName).gameObject.GetComponent<wormhole>();
+        succWormhole.outGoing += 1;     
+
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+        if (ball != null)
+        {   
+            float newAngle = -1 * Vector2.SignedAngle(Vector2.up, ball.rigidbody.velocity);
+
+            Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+            ball.rigidbody.velocity = rotation * Vector2.up * ball.rigidbody.velocity.magnitude;
+
+            Color black_color = new Color(0f, 0f, 0f, 1f);
+            Color white_color = new Color(1f, 1f, 1f, 1f);
+
+            ball.transform.position = succWormhole.transform.position;
+
+            if (ball.GetComponent<SpriteRenderer>().color == black_color) {
+                ball.GetComponent<SpriteRenderer>().color = white_color; 
             }
-            else{
-                otherWormhole = GameObject.FindWithTag("leftWormhole");
-            }
-
-            if (ball != null)
-            {   
-
-                float newAngle = -1 * Vector2.SignedAngle(Vector2.up, ball.rigidbody.velocity);
-
-                Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
-                ball.rigidbody.velocity = rotation * Vector2.up * ball.rigidbody.velocity.magnitude;
-
-                Color black_color = new Color(0f, 0f, 0f, 1f);
-                Color white_color = new Color(1f, 1f, 1f, 1f);
-
-                ball.transform.position = otherWormhole.transform.position;
-
-                if (ball.GetComponent<SpriteRenderer>().color == black_color)
-                {
-                    ball.GetComponent<SpriteRenderer>().color = white_color; 
-                }
-                else
-                {
-                    ball.GetComponent<SpriteRenderer>().color = black_color;
-                }
-                nextCollisionMinTime = Time.time + wormholeCooldownTime;
+            else {
+                ball.GetComponent<SpriteRenderer>().color = black_color;
             }
         }
+
+        Brick brick = collision.gameObject.GetComponent<Brick>();
+        if (brick != null)
+        {
+            brick.transform.position = succWormhole.transform.position;
+        }
+    
     }
 }
