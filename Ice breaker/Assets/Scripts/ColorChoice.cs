cb4a8fe0527bc1d4c9faa69ff31d7ba26fb2a9ab
Priya Mane <priya.hm@somaiya.edu>
2022-10-19 17:47:23 -0700

    Color buttons

--- Ice breaker/Assets/Scripts/ColorChoice.cs
+++ Ice breaker/Assets/Scripts/ColorChoice.cs
@@ -4,12 +4,38 @@ using UnityEngine;
 
 public class ColorChoice : MonoBehaviour
 {
-    private int color;
+    //public Button red_button;
+    //private int color;
     // Start is called before the first frame update
-   void OnMouseDown()
+   /*
+    void OnMouseDown()
    {
        Color c = this.GetComponent<SpriteRenderer>().color;
        //c = new Color(c.r* 255.0f, c.g * 255.0f, c.b * 255.0f, 1);
+       FindObjectOfType<GameManager>().SetBallColor(c);
+
+   }
+   */
+   public void SelectColorRed()
+   {
+       //Color c = this.GetComponent<SpriteRenderer>().color;
+       Color c = new Color(149f/255f, 73f/255f, 62f/255f, 1);
+       FindObjectOfType<GameManager>().SetBallColor(c);
+
+   }
+   
+   public void SelectColorBlue()
+   {
+       //Color c = this.GetComponent<SpriteRenderer>().color;
+       Color c = new Color(60f/255f, 75f/255f, 161f/255f, 1);
+       FindObjectOfType<GameManager>().SetBallColor(c);
+
+   }
+   
+   public void SelectColorYellow()
+   {
+       //Color c = this.GetComponent<SpriteRenderer>().color;
+       Color c = new Color(178f/255f, 150f/255f, 53f/255f, 1);
        FindObjectOfType<GameManager>().SetBallColor(c);
 
    }
