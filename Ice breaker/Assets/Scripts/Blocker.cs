3fa1f7cbdc95020d564109ce181e79d8c8e2bab8
Abhilash BS <bhaktara@usc.edu>
2022-10-21 07:50:55 -0700

    Timediff fix for blocker

--- Ice breaker/Assets/Scripts/Blocker.cs
+++ Ice breaker/Assets/Scripts/Blocker.cs
@@ -7,12 +7,9 @@ public class Blocker : MonoBehaviour
     
     private float rot_ang ;
 
-    private void Start(){
-        rot_ang = -6.0f * Time.deltaTime;
-    }
-
     private void Update(){
-        this.transform.Rotate(0, 0, rot_ang);
+        rot_ang =  70.0f * 0.008f;
+        this.transform.Rotate(0,0,rot_ang);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
