864fa41eb6f66db2131454734f6bd8b52478d415
Anantha S N <ananthasn@Ananthas-MacBook-Pro.local>
2022-11-27 00:52:40 -0800

    High Score with stars

--- Ice breaker/Assets/Scripts/HighScoreManager.cs
+++ Ice breaker/Assets/Scripts/HighScoreManager.cs
@@ -15,15 +15,19 @@ public class HighScoreManager : MonoBehaviour
     private Color32 color;
     private Color32 gray;
     int highScore =0;
-    public Dictionary < int, int > levelMinLivesMapping = new Dictionary < int, int > ();
+    private Dictionary < int, int > levelMinLivesMapping = new Dictionary < int, int > ();
+    private Dictionary < int, int > levelStarsMapping = new Dictionary < int, int > ();
     // // Start is called before the first frame update
     void Start()
     {
-         mapLives();
+        // PlayerPrefs.DeleteAll();
+        mapLives();
+        mapStars();
         color = new Color32(233, 225, 43, 255);
         gray = new Color32(106, 106, 106, 160);
-        if(GameManager.score> PlayerPrefs.GetInt("highScore"+ GameManager.level, 0)){
-            PlayerPrefs.SetInt("highScore"+ GameManager.level, GameManager.score);
+        int cur_score = GameManager.score + 50* GameManager.stars;
+        if(cur_score> PlayerPrefs.GetInt("highScore"+ GameManager.level, 0)){
+            PlayerPrefs.SetInt("highScore"+ GameManager.level, cur_score);
         }
         highScore = PlayerPrefs.GetInt("highScore"+ GameManager.level, 0);
         
@@ -58,8 +62,12 @@ public class HighScoreManager : MonoBehaviour
         int l = getMinLives(GameManager.level);
         float lives = GameManager.lives/(10.0f-l);
         float score = GameManager.score/ (highScore *1.0f);
-
-        return (float) Math.Round((double)(lives + score) * 3 / 2, 2);
+        float starPoint =1.0f;
+        if(getStars(GameManager.level)!=0){
+            starPoint = GameManager.stars / getStars(GameManager.level);
+        }
+        
+        return (float) Math.Round((double)(2*lives + 2*score + starPoint) * 3 / 5, 2);
 
     }
 
@@ -86,8 +94,34 @@ public class HighScoreManager : MonoBehaviour
         levelMinLivesMapping[35] = 3;
 
     }
+    private void mapStars() {
+        levelStarsMapping[1] = 0;
+        levelStarsMapping[2] = 0;
+        levelStarsMapping[3] = 0;
+        levelStarsMapping[4] = 2;
+        levelStarsMapping[5] = 2;
+        levelStarsMapping[6] = 2;
+        levelStarsMapping[7] = 2;
+        levelStarsMapping[20] = 2;
+        levelStarsMapping[8] = 2;
+        levelStarsMapping[9] = 2;
+        levelStarsMapping[10] = 2;
+        levelStarsMapping[16] = 2;
+        levelStarsMapping[18] = 2;
+        levelStarsMapping[19] = 2;
+        levelStarsMapping[15] = 2;
+        levelStarsMapping[11] = 2;
+        levelStarsMapping[12] = 2;
+        levelStarsMapping[13] = 2;
+        levelStarsMapping[17] = 2;
+        levelStarsMapping[35] = 2;
+
+    }
     public int getMinLives(int level){
         return levelMinLivesMapping[level];
+    }
+    public int getStars(int level){
+        return levelStarsMapping[level];
     }
 }
 
