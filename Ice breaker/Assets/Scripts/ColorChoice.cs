fad6c9f1f8ddccb0298bd29f8500c4cc9043652d
ss23046 <ss23046@usc.edu>
2022-11-30 20:40:50 -0800

    Commit 1

--- Ice breaker/Assets/Scripts/ColorChoice.cs
+++ Ice breaker/Assets/Scripts/ColorChoice.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColorChoice : MonoBehaviour
 {
@@ -16,6 +17,15 @@ public class ColorChoice : MonoBehaviour
 
    }
    */
+    public Button RedButton;
+    public Button BlueButton;
+    public Button YellowButton;
+    public Sprite RedNewSprite;
+    public Sprite BlueNewSprite;
+    public Sprite YellowNewSprite;
+    private int colorSet = -1;
+    private Sprite oldSprite = null;
+
     List<Color> colors = new List<Color>(){
             new Color(149f/255f, 73f/255f, 62f/255f, 1),
             new Color(60f/255f, 75f/255f, 161f/255f, 1),
@@ -27,33 +37,83 @@ public class ColorChoice : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             counter = (counter+1)%3;
+            
+            if(counter == 0){
+                if (colorSet != -1){
+                    RevertSpriteChange();
+                }
+                oldSprite =  RedButton.GetComponent<Image>().sprite;
+                RedButton.GetComponent<Image>().sprite = RedNewSprite;
+                colorSet = 0;
+            }
+            else if(counter == 1 ){
+                if (colorSet != -1){
+                    RevertSpriteChange();
+                }
+                oldSprite =  BlueButton.GetComponent<Image>().sprite;
+                BlueButton.GetComponent<Image>().sprite = BlueNewSprite;
+                colorSet = 1;
+            }
+            else if(counter == 2 ){
+                if (colorSet != -1){
+                    RevertSpriteChange();
+                }
+                oldSprite =  YellowButton.GetComponent<Image>().sprite;
+                YellowButton.GetComponent<Image>().sprite = YellowNewSprite;
+                colorSet = 2;
+            }
             FindObjectOfType<GameManager>().SetBallColor(colors[counter]);
+
         }
    }
    public void SelectColorRed()
    {
-       //Color c = this.GetComponent<SpriteRenderer>().color;
+       if (colorSet != -1){
+            RevertSpriteChange();
+        }
+       oldSprite =  RedButton.GetComponent<Image>().sprite;
        counter = 0;
+       colorSet = 0;
        Color c = new Color(149f/255f, 73f/255f, 62f/255f, 1);
        FindObjectOfType<GameManager>().SetBallColor(c);
-
+       RedButton.GetComponent<Image>().sprite= RedNewSprite;
    }
    
    public void SelectColorBlue()
    {
-       //Color c = this.GetComponent<SpriteRenderer>().color;
+        if (colorSet != -1){
+            RevertSpriteChange();
+        }
+       oldSprite =  BlueButton.GetComponent<Image>().sprite;
        counter = 1;
+       colorSet = 1;
        Color c = new Color(60f/255f, 75f/255f, 161f/255f, 1);
        FindObjectOfType<GameManager>().SetBallColor(c);
-
+        BlueButton.GetComponent<Image>().sprite= BlueNewSprite;
    }
    
    public void SelectColorYellow()
    {
-       //Color c = this.GetComponent<SpriteRenderer>().color;
+        if (colorSet != -1){
+            RevertSpriteChange();
+        }
+       oldSprite =  YellowButton.GetComponent<Image>().sprite;
        counter = 2;
+       colorSet = 2;
        Color c = new Color(178f/255f, 150f/255f, 53f/255f, 1);
        FindObjectOfType<GameManager>().SetBallColor(c);
+    YellowButton.GetComponent<Image>().sprite= YellowNewSprite;
+   }
 
+   private void RevertSpriteChange(){
+       if( colorSet == 0){
+           RedButton.GetComponent<Image>().sprite = oldSprite; 
+       }
+       else if (colorSet ==1){
+           BlueButton.GetComponent<Image>().sprite = oldSprite; 
+       }
+       else{
+           YellowButton.GetComponent<Image>().sprite = oldSprite; 
+       }
    }
 }
